caa997d36f643e6b777a27b69cfa839700d0ce22
BibekBhusal0 <bibekbhusal04@gmail.com>
2025-05-12 13:51:13 +0545

    using heroui/react

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,4 +1,4 @@
-import { SVGProps } from "react";
+import { JSX, SVGProps } from "react";
 
 export type IconDefinition = string | ((props: IconSvgProps) => JSX.Element) | JSX.Element
 export type IconSvgProps = SVGProps<SVGSVGElement> & {
