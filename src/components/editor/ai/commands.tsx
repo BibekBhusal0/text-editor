29c127bc3e43ce74c7369c19f758b3687a3dd95e
BibekBhusal0 <bibekbhusal04@gmail.com>
2025-05-17 10:23:53 +0545

    small changes in ui of ai command

--- src/components/editor/ai/commands.tsx
+++ src/components/editor/ai/commands.tsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import { Command, CommandGroup, CommandInput, CommandItem } from "@/components/ui/command";
 import { Icon } from "@/components/icons";
 import { useDummyCompletion } from "@/components/hook/useCompletion";
+import { Button } from "@heroui/button";
 
 const options = [
   {
@@ -34,87 +35,94 @@ const AISelectorCommands = () => {
   const hasCompletion = completion.trim() !== "";
 
   if (!editor) return null;
+  const getCompletion = async () => {
+    await getAiText();
+    setInputValue("");
+  }
 
   return (
 
     <Command className="w-80">
-      <CommandInput
+      <div className='flex bg-primary-50  gap-2 items-center'><CommandInput
         autoFocus
+        className='w-max-full w-56'
         placeholder="Command To AI"
         value={inputValue}
         onValueChange={setInputValue}
       />
 
-      {
-        loading ? 
-        <Icon icon="loading" />
-       : (
+        <Button size='sm' isIconOnly onPress={() => {
+          if (inputValue.trim() !== '') getCompletion
+        }} >
+          <Icon icon='send' />
+        </Button>
+      </div>
 
-        <>
-          {hasCompletion ? (
+      {
+        loading ?
+          <div className='flex items-center pl-5 gap-3 text-5xl '><Icon icon="loading" /><div className='text-lg'>Loading</div></div>
+          : (
+
             <>
-              <div>{completion}</div>
-              <CommandGroup heading="Completion actions">
-                <CommandItem
-                  onSelect={() => {
-                    setCompletion("");
-                  }}
-                >
-                  Dismiss
-                </CommandItem>
-                <CommandItem
-                  onSelect={() => {
-                    if (!editor) return;
-                    const selection = editor.view.state.selection;
-                    editor
-                      .chain()
-                      .focus()
-                      .insertContentAt(selection.to + 1, completion)
-                      .run();
-                  }}
-                >
-                  Insert
-                </CommandItem>
-                <CommandItem
-                  onSelect={() => {
-                    if (!editor) return;
-                    const selection = editor.view.state.selection;
-                    editor
-                      .chain()
-                      .focus()
-                      .insertContentAt(
-                        {
-                          from: selection.from,
-                          to: selection.to,
-                        },
-                        completion
-                      )
-                      .run();
-                  }}
-                >
-                  Replace
-                </CommandItem>
-              </CommandGroup>
+              {hasCompletion ? (
+                <>
+                  <div className='h-auto max-h-40 p-2 overflow-auto'>{completion}</div>
+                  <CommandGroup heading="Completion actions">
+                    <CommandItem onSelect={() => setCompletion("")}>
+                      Dismiss
+                    </CommandItem>
+                    <CommandItem
+                      onSelect={() => {
+                        if (!editor) return;
+                        const selection = editor.view.state.selection;
+                        editor
+                          .chain()
+                          .focus()
+                          .insertContentAt(selection.to + 1, completion)
+                          .run();
+                      }}
+                    >
+                      Insert
+                    </CommandItem>
+                    <CommandItem
+                      onSelect={() => {
+                        if (!editor) return;
+                        const selection = editor.view.state.selection;
+                        editor
+                          .chain()
+                          .focus()
+                          .insertContentAt(
+                            {
+                              from: selection.from,
+                              to: selection.to,
+                            },
+                            completion
+                          )
+                          .run();
+                      }}
+                    >
+                      Replace
+                    </CommandItem>
+                  </CommandGroup>
+                </>
+
+              ) : (
+
+                <CommandGroup heading="AI commands">
+                  {options.map((option) => (
+                    <CommandItem
+                      onSelect={getCompletion}
+                      className="flex gap-2 px-4"
+                      key={option.value}
+                    >
+                      <Icon className="text-foreground" icon={option.icon} size={30} />
+                      {option.label}
+                    </CommandItem>
+                  ))}
+                </CommandGroup>
+              )}
             </>
-          ) : (
-            <CommandGroup heading="AI commands">
-              {options.map((option) => (
-                <CommandItem
-                  onSelect={async () => {
-                    await getAiText();
-                    setInputValue("");
-                  }}
-                  className="flex gap-2 px-4"
-                  key={option.value}
-                >
-                  <Icon className="text-foreground" icon={option.icon} size={30} />
-                  {option.label}
-                </CommandItem>
-              ))}
-            </CommandGroup>
           )}
-        </>
-      )}
     </Command>
   );
 };
